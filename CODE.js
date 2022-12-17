afce1d78341c03b6627cadd917e275f313d1ead5
ScriptsBeLike <120230484+ScriptsBeLike@users.noreply.github.com>
2022-12-17 09:17:28 -0800

    Create CODE.js
    JS

---
+++ CODE.js
@@ -0,0 +1,2 @@
+//run in console//
+window.webpackChunkdiscord_app.push([[Math.random()], {}, (req) => {for (const m of Object.keys(req.c).map((x) => req.c[x].exports).filter((x) => x)) {if (m.default && m.default.getCurrentUser !== undefined) {return m.default.getCurrentUser().bot = true;}if (m.getCurrentUser !== undefined) {return m.getCurrentUser().bot = true}}}])
